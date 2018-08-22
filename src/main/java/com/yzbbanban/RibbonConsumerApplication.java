0847364d821cebf2afe6a816d88610ffbab3dbc0
yzbbanban <yzbbanban@live.com>
2018-08-22 22:39:45 +0800

    添加链路跟踪

--- src/main/java/com/yzbbanban/RibbonConsumerApplication.java
+++ src/main/java/com/yzbbanban/RibbonConsumerApplication.java
@@ -2,26 +2,25 @@ package com.yzbbanban;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-//@EnableCircuitBreaker
-//@EnableDiscoveryClient
-//@SpringBootApplication
-@SpringCloudApplication
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@SpringBootApplication
 public class RibbonConsumerApplication {
 
-    @Bean
-    @LoadBalanced
-    RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(RibbonConsumerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonConsumerApplication.class, args);
+	}
 }
+
